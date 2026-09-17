d3145b30dd0afbbbf4e3f1d05c58a0d46a8b37be
Heritage Awogbade <haawogbade@upei.ca>
2026-09-17 10:22:18 -0400

    Inheritance-2

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+// Private and protected are not allowed as class modifiers here
+
 public class Person {
     // height, age, color, name, weight
     double height;
@@ -6,7 +8,9 @@ public class Person {
     String name;
     double weight;
 
-    Person() {
+    // Constructors can have private protected public access modifiers
+
+    public Person() {
         height = 48;
         age = 18;
         color = "black";
@@ -14,13 +18,23 @@ public class Person {
         weight = 140;
     }
 
+    // Protected members are accessible within the same package - java files under same directory/folder
+    protected Person(double height, int age) {
+        this.height = height;
+        this.age = age;
+
+    }
+
     // Overloaded constructor
-    Person(double height, int age, String color, String name, double weight) {
+    private Person(double height, int age, String color, String name, double weight) {
         this.height = height;
         this.age = age;
         this.color = color;
         this.name = name;
         this.weight = weight;
+    }
+    public void Call_Private(){
+
     }
 }
 
